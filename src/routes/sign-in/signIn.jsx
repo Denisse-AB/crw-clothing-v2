f394e5ae20b127bd635987e0fa069f12f32a8106
Denisse Abreu <denisseabreu21@yahoo.com>
2022-03-17 19:44:48 -0400

    firebase, navigation bar  and routing

---
+++ src/routes/sign-in/signIn.jsx
@@ -0,0 +1,17 @@
+import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase";
+
+const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup()
+    const userDocRef = createUserDocumentFromAuth(user)
+    console.log(user)
+  }
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign In With Google</button>
+    </div>
+  );
+}
+
+export default SignIn;
